6a71841aae4f7dd691e065643061b1bef23dc6d3
dhayden <cdhayden25@gmail.com>
2015-01-31 20:13:19 -0600

    rpc

--- 2015_RPC_DistSystems/main.c
+++ 2015_RPC_DistSystems/main.c
@@ -5,11 +5,13 @@
 //  Created by Derrick Hayden on 1/31/15.
 //  Copyright (c) 2015 Derrick Hayden. All rights reserved.
 //
-
+#include <rpc/rpc.h>
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    
+    
     printf("Hello, World!\n");
     return 0;
 }
